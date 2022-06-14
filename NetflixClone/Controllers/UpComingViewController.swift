0d61b571f1958d043e41a4bc250be2ff2456fff7
vuhm <vuhm@vietis.com.vn>
2022-06-14 11:01:49 +0700

    create tab bar view

---
+++ NetflixClone/Controllers/UpComingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  UpComingViewController.swift
+//  NetflixClone
+//
+//  Created by it on 14/06/22.
+//
+
+import UIKit
+
+class UpComingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+}
